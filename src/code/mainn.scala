bddafb925697b890c5cb662dec8c1bab6990b059
Amey Chavan <amey.chavan@vrindasoftech.com>
2014-09-01 13:21:16 +0530

    Init 1

---
+++ src/code/mainn.scala
@@ -0,0 +1,13 @@
+package code;
+
+object mainn {
+
+  def main(args: Array[String]): Unit = {
+    // var a="Hello world"
+
+    println("---------------------------------------")
+    println("heloooo")
+
+  }
+
+}
